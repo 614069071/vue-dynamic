1e2e08d8bb4bd38eaf5112d9b039e142208e0955
JinTong <tong.jin@cdatatec.com>
2021-01-29 15:54:35 +0800

    设备在线率颜色修改

--- src/views/resource/components/online.vue
+++ src/views/resource/components/online.vue
@@ -6,63 +6,80 @@
 <script>
 import * as Echarts from 'echarts';
 
-// 设备在线
-var onlineOption = {
-	title: [
-		{
-			text: '设备在线率',
-			left: '0',
-			textStyle: {
-				color: '#436EEE',
-				fontSize: 15,
-			},
-		},
-		{
-			subtext: '在线总数：25411台',
-			left: '16.67%',
-			top: '75%',
-			textAlign: 'center',
-		},
-	],
-	tooltip: {
-		trigger: 'item',
-	},
-	legend: {
-		// orient: 'vertical',
-		right: '0',
-	},
-	series: [
-		{
-			name: '访问来源',
-			type: 'pie',
-			radius: ['40%', '70%'],
-			avoidLabelOverlap: false,
-			label: {
-				show: false,
-				position: 'center',
-			},
-			emphasis: {
-				label: {
-					show: true,
-					fontSize: '20',
-					fontWeight: 'bold',
-				},
-			},
-			labelLine: {
-				show: false,
-			},
-			data: [
-				{ value: 1048, name: '在线设备' },
-				{ value: 735, name: '离线设备' },
-			],
-		},
-	],
-};
-
 export default {
 	components: {},
 	data() {
-		return {};
+		return {
+			scale: '90%',
+		};
+	},
+	computed: {
+		options() {
+			return {
+				title: [
+					{
+						text: '设备在线率',
+						left: '0',
+						textStyle: {
+							color: '#436EEE',
+							fontSize: 15,
+						},
+					},
+					{
+						subtext: '在线总数：25411台',
+						left: '16.67%',
+						top: '75%',
+						textAlign: 'center',
+					},
+				],
+				color: ['#409EFF', '#DCDFE6'],
+				tooltip: {
+					trigger: 'item',
+				},
+				graphic: {
+					//图形中间文字
+					type: 'text',
+					left: 'center',
+					top: 'center',
+					style: {
+						text: this.scale,
+						textAlign: 'center',
+						fill: '#010101',
+						fontSize: 28,
+					},
+				},
+				legend: {
+					// orient: 'vertical',
+					right: '0',
+				},
+				series: [
+					{
+						name: '访问来源',
+						type: 'pie',
+						radius: ['40%', '70%'],
+						avoidLabelOverlap: false,
+						label: {
+							show: false,
+							position: 'center',
+						},
+						emphasis: {
+							label: {
+								// show: true,
+								fontSize: '20',
+								fontWeight: 'bold',
+							},
+						},
+						labelLine: {
+							show: false,
+						},
+						data: [
+							{ value: 90, name: '在线设备' },
+							{ value: 10, name: '离线设备' },
+						],
+					},
+				],
+			};
+		},
 	},
 	mounted() {
 		this.initOnlineEcharts();
@@ -74,8 +91,9 @@ export default {
 		},
 
 		initOnlineEcharts() {
-			const main = this.$refs.resource_online;
-			this.setEchart(main, onlineOption);
+			const el = this.$refs.resource_online;
+			const echarts = Echarts.init(el);
+			echarts.setOption(this.options);
 		},
 	},
 };
