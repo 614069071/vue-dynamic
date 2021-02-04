d2c3363f5cf2a1dabd205a3b44c77f73f2aaf13c
JinTong <tong.jin@cdatatec.com>
2021-02-04 14:35:36 +0800

    u

--- src/main.js
+++ src/main.js
@@ -26,9 +26,9 @@ router.beforeEach((to, from, next) => {
 });
 
 router.afterEach((to, from) => {
-  var routers = to.matched;
-  var arr = routers.length && routers.filter((route) => route.parent && route.parent.path && route.parent.path != '/');
-  var route = arr[arr.length - 1];
+  const routers = to.matched;
+  const arr = routers.length && routers.filter((route) => route.parent && route.parent.path && route.parent.path != '/');
+  const route = arr[arr.length - 1];
 
   route && store.commit('UPDATE_CACHE_ROUTER', { name: route.name, path: route.path, title: route.meta.name });
   route && store.commit('UPDATE_DEFAULT_ACTIVE', route.path);
