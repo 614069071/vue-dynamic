33d9d6a679fb01fabfa16736e75490ca492be55e
JinTong <tong.jin@cdatatec.com>
2021-02-25 19:36:51 +0800

    u

--- src/views/log/inquire.vue
+++ src/views/log/inquire.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="log-inquire-view-wrapper view-wrapper">
 		<div class="inner-header-wrapper">
-			<el-form inline label-width="80px" :model="queryFormData">
+			<el-form inline label-width="80px" :model="inquireFormData">
 				<el-form-item label="日期：">
-					<el-date-picker type="date" placeholder="选择日期" v-model="queryFormData.name1"></el-date-picker>
+					<el-date-picker type="date" placeholder="选择日期" v-model="inquireFormData.name1"></el-date-picker>
 				</el-form-item>
 				<el-form-item>
 					<el-button type="primary">查询</el-button>
@@ -36,7 +36,7 @@ export default {
 	name: 'log-inquire',
 	data() {
 		return {
-			queryFormData: {},
+			inquireFormData: {},
 			dataTable: [
 				{
 					name1: '1',
